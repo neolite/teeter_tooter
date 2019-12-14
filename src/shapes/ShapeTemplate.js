cf78922eecb8cb956806fec72f0353b3d16fe925
Rafkat <ufadiz@gmail.com>
2019-12-14 23:43:52 +0300

    Add group to template

--- src/shapes/ShapeTemplate.js
+++ src/shapes/ShapeTemplate.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Text } from "react-konva";
+import { Text, Group } from "react-konva";
 
 const ShapeTemplate = ({ children, component, text, ...rest }) => {
   return (
-    <>
+    <Group>
       <Text text={text} />
       {React.createElement(component, rest, children)}
-    </>
+    </Group>
   );
 };
 
