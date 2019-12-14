7c44653f494d478f8a7ec2c6347bd11354479fe9
Rafkat <ufadiz@gmail.com>
2019-12-14 19:17:39 +0300

    Add shapes & state manager

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import './App.css';
+import { Stage, Layer, Text } from 'react-konva';
+
+import ColoredRect from './shapes/ColoredRect';
+import Triangle from './shapes/Triangle';
+import Line from './shapes/Line';
+
+const PX_TO_METERS_RATIO = 50;
+const METER = 1;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer    
+        x={300}
+        y={300}
+      >
+        <Text text="Try click on rect" />
+        <ColoredRect />
+        <Line x1={200} y1={20} x2={600} y2={20} />
+        <Triangle x={400} y={100} />
+      </Layer>
+  </Stage>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useStrictMode } from 'react-konva';
+
 import './index.css';
 import App from './App';
+
+useStrictMode(true);
 
 ReactDOM.render(<App />, document.getElementById('root'));
